75f8dc46240de8684baf71b4534a67b2bdf44074
m-mngadi <mngadimziwenhlanhla@gmail.com>
2023-04-07 15:33:55 +0200

    Initial commit

---
+++ app/src/test/java/com/codesurfers/clinicapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.codesurfers.clinicapp
+
+import com.codesurfers.clinicapp.appcomponents.base.BaseActivity
+import com.codesurfers.clinicapp.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
